375e7f0e376b6fd3dc35b5df4b4cb595ce55e9c0
Rekecki Petra <h163296@stud.u-szeged.hu>
2024-02-08 12:17:24 +0100

    create layout

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron text-center">
+    <h1>Welcome To Library</h1>
+    <p><a class="btn btn-primary btn-lg" href="/login">Login</a>
+        <a class="btn btn-primary btn-lg" href="/register">Register</a></p>
+</div>
+@endsection
